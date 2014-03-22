5c1f8901b6424386c9bbd08649c2d119f13bd194
danilau <danilau@tut.by>
2014-03-22 12:26:52 -0700

    009 Add FTAXMLParser:NSXMLParser

---
+++ ForteTestApplication/RSS/FTAXMLParser.h
@@ -0,0 +1,13 @@
+//
+//  FTAXMLParser.h
+//  ForteTestApplication
+//
+//  Created by majstrak on 22.03.14.
+//  Copyright (c) 2014 Danilau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTAXMLParser : NSXMLParser
+
+@end
---
+++ ForteTestApplication/RSS/FTAXMLParser.m
@@ -0,0 +1,13 @@
+//
+//  FTAXMLParser.m
+//  ForteTestApplication
+//
+//  Created by majstrak on 22.03.14.
+//  Copyright (c) 2014 Danilau. All rights reserved.
+//
+
+#import "FTAXMLParser.h"
+
+@implementation FTAXMLParser
+
+@end
