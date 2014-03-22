ce9f22ba055fee0ded8c81c1f0cb5dca79b2075a
danilau <danilau@tut.by>
2014-03-22 10:22:49 -0700

    Initial Commit

---
+++ ForteTestApplication/FTAAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  FTAAppDelegate.h
+//  ForteTestApplication
+//
+//  Created by majstrak on 22.03.14.
+//  Copyright (c) 2014 Danilau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
